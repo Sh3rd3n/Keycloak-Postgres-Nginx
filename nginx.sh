6e0ea3a08c7708832159c410e589de11ec6a9c20
Luca <luca.derosas@hotmail.com>
2024-03-27 11:13:52 +0100

    update

--- nginx.sh
+++ nginx.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env zsh
+#!/usr/bin/bash
 
 cd app
-docker-compose down --remove-orphans
-docker-compose build
-docker-compose up -d
+docker compose down --remove-orphans
+docker compose build
+docker compose up -d
 
 echo "Success!"
 
